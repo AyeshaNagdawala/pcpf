1bb8371026190ba38042a3ab4851852647dda0c3
AyeshaNagdawala <111572744+AyeshaNagdawala@users.noreply.github.com>
2022-11-02 21:06:10 +0530

    Update AreaOfTri.pl

--- AreaOfTri.pl
+++ AreaOfTri.pl
@@ -1,11 +1,11 @@
 math:-
-   write('Enter the three sides of a triangle:\n ),
+   write('Enter the three sides of a triangle:\n '),
    read(A),
    read(B),
    read(C),
    S is (A+B+C)/2,
    Area is sqrt((S-A)*(S-B)*(S-C)),
-   write('Area is ),
+   write('Area is '),
    write(Area).
 
 
