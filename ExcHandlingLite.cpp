a35cb333120cf1e833079b66a569e28cdd2b5d42
AyeshaNagdawala <111572744+AyeshaNagdawala@users.noreply.github.com>
2022-11-04 01:41:45 +0530

    Create ExcHandlingLite.cpp

---
+++ ExcHandlingLite.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        int a=12;
+        int b=0;
+        if(b!=0)
+        {
+            cout<<"Division is : "<<(a/b);
+        }
+        else
+        {
+            cout<<"Division by zero exception.";
+        }
+    }
+
