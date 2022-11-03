ee41ef5b9e3cb1060842720656f47950fb709e09
AyeshaNagdawala <111572744+AyeshaNagdawala@users.noreply.github.com>
2022-11-04 01:31:05 +0530

    Create exceptionHandling.java

---
+++ exceptionHandling.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String args[])
+    {
+        int a=8;
+        int b=0;
+        try
+        {System.out.println(a/b);}
+        catch(Exception e)
+         {System.out.println("Division by zero");}
+            
+    }
+}
